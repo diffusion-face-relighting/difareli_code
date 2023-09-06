d7c0abe3726ac0c2a0f53c41250e0e09842fb34e
xezxey <puntawat-1@hotmail.com>
2023-09-06 15:32:05 +0700

    up

--- guided_diffusion/models/spatial_cond_arch/unet_spatial_condition_hadamart.py
+++ guided_diffusion/models/spatial_cond_arch/unet_spatial_condition_hadamart.py
@@ -1593,3 +1593,257 @@ class UNetModel_SpatialCondition_Hadamart(nn.Module):
         assert len(apply_cond_layer) == 0
         return {'output':self.out(h)}
         
+
+class EncoderUNet_SpatialCondition(nn.Module):
+    """
+    The half UNet model with attention.
+    For usage, see UNet.
+    """
+
+    def __init__(
+        self,
+        image_size,
+        in_channels,
+        model_channels,
+        out_channels,
+        num_res_blocks,
+        attention_resolutions,
+        conditioning,
+        condition_dim,
+        dropout=0,
+        channel_mult=(1, 2, 4, 8),
+        conv_resample=True,
+        dims=2,
+        use_checkpoint=False,
+        use_fp16=False,
+        num_heads=1,
+        num_head_channels=-1,
+        num_heads_upsample=-1,
+        use_scale_shift_norm=False,
+        resblock_updown=False,
+        use_new_attention_order=False,
+        pool="adaptive",
+    ):
+        super().__init__()
+
+        if num_heads_upsample == -1:
+            num_heads_upsample = num_heads
+
+        self.image_size = image_size
+        self.in_channels = in_channels
+        self.model_channels = model_channels
+        self.out_channels = out_channels
+        self.num_res_blocks = num_res_blocks
+        self.attention_resolutions = attention_resolutions
+        self.dropout = dropout
+        self.channel_mult = channel_mult
+        self.conv_resample = conv_resample
+        self.use_checkpoint = use_checkpoint
+        self.dtype = th.float16 if use_fp16 else th.float32
+        self.num_heads = num_heads
+        self.num_head_channels = num_head_channels
+        self.num_heads_upsample = num_heads_upsample
+        self.conditioning = conditioning
+        self.condition_dim = condition_dim
+
+        time_embed_dim = model_channels * 4
+
+        ch = int(channel_mult[0] * model_channels)
+        self.input_blocks = nn.ModuleList(
+            [TimestepEmbedSequential(conv_nd(dims, in_channels, ch, 3, padding=1))]
+        )
+        self._feature_size = ch
+        input_block_chans = [ch]
+        ds = 1
+        for level, mult in enumerate(channel_mult):
+            for _ in range(num_res_blocks):
+                layers = [
+                    ResBlockNoTime(
+                        ch,
+                        time_embed_dim,
+                        dropout,
+                        out_channels=int(mult * model_channels),
+                        dims=dims,
+                        use_checkpoint=use_checkpoint,
+                        use_scale_shift_norm=use_scale_shift_norm,
+                    )
+                ]
+                ch = int(mult * model_channels)
+                if ds in attention_resolutions:
+                    layers.append(
+                        AttentionBlock(
+                            ch,
+                            use_checkpoint=use_checkpoint,
+                            num_heads=num_heads,
+                            num_head_channels=num_head_channels,
+                            use_new_attention_order=use_new_attention_order,
+                        )
+                    )
+                self.input_blocks.append(TimestepEmbedSequential(*layers))
+                self._feature_size += ch
+                input_block_chans.append(ch)
+            if level != len(channel_mult) - 1:
+                out_ch = ch
+                self.input_blocks.append(
+                    TimestepEmbedSequential(
+                        ResBlockNoTime(
+                            ch,
+                            time_embed_dim,
+                            dropout,
+                            out_channels=out_ch,
+                            dims=dims,
+                            use_checkpoint=use_checkpoint,
+                            use_scale_shift_norm=use_scale_shift_norm,
+                            down=True,
+                        )
+                        if resblock_updown
+                        else Downsample(
+                            ch, conv_resample, dims=dims, out_channels=out_ch
+                        )
+                    )
+                )
+                ch = out_ch
+                input_block_chans.append(ch)
+                ds *= 2
+                self._feature_size += ch
+
+        self.middle_block = TimestepEmbedSequential(
+            ResBlockNoTime(
+                ch,
+                time_embed_dim,
+                dropout,
+                dims=dims,
+                use_checkpoint=use_checkpoint,
+                use_scale_shift_norm=use_scale_shift_norm,
+            ),
+            AttentionBlock(
+                ch,
+                use_checkpoint=use_checkpoint,
+                num_heads=num_heads,
+                num_head_channels=num_head_channels,
+                use_new_attention_order=use_new_attention_order,
+            ),
+            ResBlockNoTime(
+                ch,
+                time_embed_dim,
+                dropout,
+                dims=dims,
+                use_checkpoint=use_checkpoint,
+                use_scale_shift_norm=use_scale_shift_norm,
+            ),
+        )
+
+        # print("ENCODER")
+        # for i in range(len(self.input_blocks)):
+        #     print(i, self.input_blocks[i])
+        # exit()
+        # print(self.middle_block)
+        self._feature_size += ch
+
+        """
+        self.pool = pool
+        if pool == "adaptive":
+            self.out = nn.Sequential(
+                normalization(ch),
+                nn.SiLU(),
+                nn.AdaptiveAvgPool2d((1, 1)),
+                zero_module(conv_nd(dims, ch, out_channels, 1)),
+                nn.Flatten(),
+            )
+        elif pool == "adaptivenonzero":
+            self.out = nn.Sequential(
+                normalization(ch),
+                nn.SiLU(),
+                nn.AdaptiveAvgPool2d((1, 1)),
+                conv_nd(dims, ch, out_channels, 1),
+                nn.Flatten(),
+            )
+        elif pool == "adaptivenonzero_norm1":
+            self.out = nn.Sequential(
+                normalization(ch),
+                nn.SiLU(),
+                nn.AdaptiveAvgPool2d((1, 1)),
+                conv_nd(dims, ch, out_channels, 1),
+                nn.Flatten(),
+                Norm(ord=1),
+            )
+        elif pool == "attention":
+            assert num_head_channels != -1
+            self.out = nn.Sequential(
+                normalization(ch),
+                nn.SiLU(),
+                AttentionPool2d(
+                    (image_size // ds), ch, num_head_channels, out_channels
+                ),
+            )
+        elif pool == "spatial":
+            self.out = nn.Sequential(
+                nn.Linear(self._feature_size, 2048),
+                nn.ReLU(),
+                nn.Linear(2048, self.out_channels),
+            )
+        elif pool == "spatial_tanh":
+            self.out = nn.Sequential(
+                nn.Linear(self._feature_size, 2048),
+                nn.ReLU(),
+                nn.Linear(2048, self.out_channels),
+                nn.Tanh()
+            )
+        elif pool == "spatial_relu":
+            self.out = nn.Sequential(
+                nn.Linear(self._feature_size, 2048),
+                nn.ReLU(),
+                nn.Linear(2048, self.out_channels),
+                nn.ReLU()
+            )
+        elif pool == "spatial_mlp":
+            self.out = nn.Sequential(
+                nn.Linear(self._feature_size, 2048),
+                nn.ReLU(),
+                nn.Linear(2048, 1024),
+                nn.ReLU(),
+                nn.Linear(1024, 512),
+                nn.ReLU(),
+                nn.Linear(512, self.out_channels),
+            )
+        elif pool == "spatial_v2":
+            self.out = nn.Sequential(
+                nn.Linear(self._feature_size, 2048),
+                normalization(2048),
+                nn.SiLU(),
+                nn.Linear(2048, self.out_channels),
+            )
+        else:
+            raise NotImplementedError(f"Unexpected {pool} pooling")
+        """
+
+    def convert_to_fp16(self):
+        """
+        Convert the torso of the model to float16.
+        """
+        self.input_blocks.apply(convert_module_to_f16)
+        self.middle_block.apply(convert_module_to_f16)
+
+    def convert_to_fp32(self):
+        """
+        Convert the torso of the model to float32.
+        """
+        self.input_blocks.apply(convert_module_to_f32)
+        self.middle_block.apply(convert_module_to_f32)
+
+    def forward(self, x, emb=None):
+        """
+        Apply the model to an input batch.
+        :param x: an [N x C x ...] Tensor of inputs.
+        :param timesteps: a 1-D batch of timesteps.
+        :return: an [N x K] Tensor of outputs.
+        """
+        results = []
+        h = x.type(self.dtype)
+        for _, module in enumerate(self.input_blocks):
+            h = module(h, emb)
+            results.append(h)
+        h = self.middle_block(h, emb)
+        results.append(h)
+        return results
+        
--- guided_diffusion/script_util.py
+++ guided_diffusion/script_util.py
@@ -4,14 +4,7 @@ import argparse
 from . import gaussian_diffusion as gd
 from guided_diffusion.respace import SpacedDiffusion, space_timesteps
 from guided_diffusion.models.unet import EncoderUNetModelNoTime, UNetModelCondition, UNetModel
-from guided_diffusion.models.spatial_cond_arch.unet_spatial_condition_concat import EncoderUNet_SpatialCondition, UNetModel_SpatialCondition_Concat
-from guided_diffusion.models.spatial_cond_arch.unet_spatial_condition_hadamart import UNetModel_SpatialCondition_Hadamart
-from guided_diffusion.models.spatial_cond_arch.unet_spatial_condition_hadamart_no_dpm import UNetModel_SpatialCondition_Hadamart_No_DPM
-from guided_diffusion.models.spatial_cond_arch.unet_spatial_condition_hadamart_no_dpm_notime import UNetModel_SpatialCondition_Hadamart_No_DPM_NoTime
-from guided_diffusion.models.spatial_cond_arch.unet_spatial_condition_hadamart_sameemb import UNetModel_SpatialCondition_Hadamart_SameEmb
-from guided_diffusion.models.unet_duplicate import UNetModelConditionDuplicate
-from guided_diffusion.models.normals_arch.unet_N_Last import UNetNormalsLastLayer
-from guided_diffusion.models.normals_arch.unet_N_All import UNetNormalsAll
+from guided_diffusion.models.spatial_cond_arch.unet_spatial_condition_hadamart import UNetModel_SpatialCondition_Hadamart, EncoderUNet_SpatialCondition
 from guided_diffusion.models.dense import DenseDDPM, AutoEncoderDPM, DenseDDPMCond
 
 NUM_CLASSES = 1000
@@ -106,50 +99,6 @@ def create_model(cfg, all_cfg=None):
             resblock_updown=cfg.resblock_updown,
             use_new_attention_order=cfg.use_new_attention_order,
         )
-    elif cfg.arch == 'UNetNormalsLastLayer':
-        return UNetNormalsLastLayer(
-            image_size=cfg.image_size,
-            in_channels=cfg.in_channels,
-            model_channels=cfg.num_channels,
-            out_channels=cfg.out_channels,
-            num_res_blocks=cfg.num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=cfg.dropout,
-            channel_mult=channel_mult,
-            use_checkpoint=cfg.use_checkpoint,
-            num_heads=cfg.num_heads,
-            num_head_channels=cfg.num_head_channels,
-            num_heads_upsample=cfg.num_heads_upsample,
-            use_scale_shift_norm=cfg.use_scale_shift_norm,
-            resblock_updown=cfg.resblock_updown,
-            use_new_attention_order=cfg.use_new_attention_order,
-            condition_dim=cfg.condition_dim,
-            condition_proj_dim=cfg.condition_proj_dim,
-            conditioning=cfg.conditioning,
-            all_cfg=all_cfg,
-        )
-    elif cfg.arch == 'UNetNormalsAll':
-        return UNetNormalsAll(
-            image_size=cfg.image_size,
-            in_channels=cfg.in_channels,
-            model_channels=cfg.num_channels,
-            out_channels=cfg.out_channels,
-            num_res_blocks=cfg.num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=cfg.dropout,
-            channel_mult=channel_mult,
-            use_checkpoint=cfg.use_checkpoint,
-            num_heads=cfg.num_heads,
-            num_head_channels=cfg.num_head_channels,
-            num_heads_upsample=cfg.num_heads_upsample,
-            use_scale_shift_norm=cfg.use_scale_shift_norm,
-            resblock_updown=cfg.resblock_updown,
-            use_new_attention_order=cfg.use_new_attention_order,
-            condition_dim=cfg.condition_dim,
-            condition_proj_dim=cfg.condition_proj_dim,
-            conditioning=cfg.conditioning,
-            all_cfg=all_cfg,
-        )
     elif cfg.arch == 'UNetCond':
         return UNetModelCondition(
             image_size=cfg.image_size,
@@ -192,29 +141,6 @@ def create_model(cfg, all_cfg=None):
             conditioning=True,
             pool=cfg.pool
         )
-    elif cfg.arch == 'UNetCondDuplicate':
-            return UNetModelConditionDuplicate(
-            image_size=cfg.image_size,
-            in_channels=cfg.in_channels,
-            model_channels=cfg.num_channels,
-            out_channels=cfg.out_channels,
-            num_res_blocks=cfg.num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=cfg.dropout,
-            channel_mult=channel_mult,
-            use_checkpoint=cfg.use_checkpoint,
-            num_heads=cfg.num_heads,
-            num_head_channels=cfg.num_head_channels,
-            num_heads_upsample=cfg.num_heads_upsample,
-            use_scale_shift_norm=cfg.use_scale_shift_norm,
-            resblock_updown=cfg.resblock_updown,
-            use_new_attention_order=cfg.use_new_attention_order,
-            condition_dim=cfg.condition_dim,
-            condition_proj_dim=cfg.condition_proj_dim,
-            conditioning=True,
-            num_SH=all_cfg.relighting.num_SH,
-            last_conv=cfg.last_conv
-        )
     elif cfg.arch == 'UNetCond_SpatialCondition_Hadamart':
         return UNetModel_SpatialCondition_Hadamart(
             image_size=cfg.image_size,
@@ -235,95 +161,6 @@ def create_model(cfg, all_cfg=None):
             condition_dim=cfg.condition_dim,
             condition_proj_dim=cfg.condition_proj_dim,
             conditioning=cfg.conditioning,
-            all_cfg=all_cfg,
-        )
-    elif cfg.arch == 'UNetCond_SpatialCondition_Hadamart_No_DPM':
-        return UNetModel_SpatialCondition_Hadamart_No_DPM(
-            image_size=cfg.image_size,
-            in_channels=cfg.in_channels,
-            model_channels=cfg.num_channels,
-            out_channels=cfg.out_channels,
-            num_res_blocks=cfg.num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=cfg.dropout,
-            channel_mult=channel_mult,
-            use_checkpoint=cfg.use_checkpoint,
-            num_heads=cfg.num_heads,
-            num_head_channels=cfg.num_head_channels,
-            num_heads_upsample=cfg.num_heads_upsample,
-            use_scale_shift_norm=cfg.use_scale_shift_norm,
-            resblock_updown=cfg.resblock_updown,
-            use_new_attention_order=cfg.use_new_attention_order,
-            condition_dim=cfg.condition_dim,
-            condition_proj_dim=cfg.condition_proj_dim,
-            conditioning=cfg.conditioning,
-            all_cfg=all_cfg,
-        )
-        
-    elif cfg.arch == 'UNetCond_SpatialCondition_Hadamart_No_DPM_NoTime':
-        return UNetModel_SpatialCondition_Hadamart_No_DPM_NoTime(
-            image_size=cfg.image_size,
-            in_channels=cfg.in_channels,
-            model_channels=cfg.num_channels,
-            out_channels=cfg.out_channels,
-            num_res_blocks=cfg.num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=cfg.dropout,
-            channel_mult=channel_mult,
-            use_checkpoint=cfg.use_checkpoint,
-            num_heads=cfg.num_heads,
-            num_head_channels=cfg.num_head_channels,
-            num_heads_upsample=cfg.num_heads_upsample,
-            use_scale_shift_norm=cfg.use_scale_shift_norm,
-            resblock_updown=cfg.resblock_updown,
-            use_new_attention_order=cfg.use_new_attention_order,
-            condition_dim=cfg.condition_dim,
-            condition_proj_dim=cfg.condition_proj_dim,
-            conditioning=cfg.conditioning,
-            all_cfg=all_cfg,
-        )
-    elif cfg.arch == 'UNetCond_SpatialCondition_Hadamart_SameEmb':
-        return UNetModel_SpatialCondition_Hadamart_SameEmb(
-            image_size=cfg.image_size,
-            in_channels=cfg.in_channels,
-            model_channels=cfg.num_channels,
-            out_channels=cfg.out_channels,
-            num_res_blocks=cfg.num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=cfg.dropout,
-            channel_mult=channel_mult,
-            use_checkpoint=cfg.use_checkpoint,
-            num_heads=cfg.num_heads,
-            num_head_channels=cfg.num_head_channels,
-            num_heads_upsample=cfg.num_heads_upsample,
-            use_scale_shift_norm=cfg.use_scale_shift_norm,
-            resblock_updown=cfg.resblock_updown,
-            use_new_attention_order=cfg.use_new_attention_order,
-            condition_dim=cfg.condition_dim,
-            condition_proj_dim=cfg.condition_proj_dim,
-            conditioning=cfg.conditioning,
-            all_cfg=all_cfg,
-        )
-    elif cfg.arch == 'UNetCond_SpatialCondition_Concat':
-        return UNetModel_SpatialCondition_Concat(
-            image_size=cfg.image_size,
-            in_channels=cfg.in_channels,
-            model_channels=cfg.num_channels,
-            out_channels=cfg.out_channels,
-            num_res_blocks=cfg.num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=cfg.dropout,
-            channel_mult=channel_mult,
-            use_checkpoint=cfg.use_checkpoint,
-            num_heads=cfg.num_heads,
-            num_head_channels=cfg.num_head_channels,
-            num_heads_upsample=cfg.num_heads_upsample,
-            use_scale_shift_norm=cfg.use_scale_shift_norm,
-            resblock_updown=cfg.resblock_updown,
-            use_new_attention_order=cfg.use_new_attention_order,
-            condition_dim=cfg.condition_dim,
-            condition_proj_dim=cfg.condition_proj_dim,
-            conditioning=True,
             all_cfg=all_cfg,
         )
     elif cfg.arch == 'EncoderUNet_SpatialCondition':
